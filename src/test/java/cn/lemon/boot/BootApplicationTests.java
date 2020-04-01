1c438753a76cefdfc6c3c3c628cc83cd378a82c5
Lemon <Jiangxinyun2012@163.com>
2020-04-01 16:34:02 +0800

    springboot添加redis配置，并编写简单的测试类

--- src/test/java/cn/lemon/boot/BootApplicationTests.java
+++ src/test/java/cn/lemon/boot/BootApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.lemon.boot;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import cn.lemon.boot.dao.StudentDao;
 import cn.lemon.boot.dto.StudentDto;
 import cn.lemon.boot.service.StudentService;
@@ -7,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,6 +17,9 @@ public class BootApplicationTests {
 
     @Autowired
     private StudentService studentService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Test
     public void contextLoads() {
     }
@@ -23,5 +28,17 @@ public class BootApplicationTests {
     public void testStrudent(){
         StudentDto studentDto = studentService.getStudentById(1);
         System.out.println(studentDto.toString());
+    }
+
+    /**测试redis写入功能*/
+    @Test
+    public void testRedisAdd(){
+        redisTemplate.opsForValue().set("name","Lemon");
+    }
+    /**测试redis的get功能*/
+    @Test
+    public void testRedisGet(){
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("得到redis的值：" + name);
     }
 }
