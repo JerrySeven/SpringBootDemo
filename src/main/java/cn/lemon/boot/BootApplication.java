bfd2e47174bf5672df901cd6ebe1e449f2b972f5
Lemon <Jiangxinyun2012@163.com>
2020-04-02 15:44:34 +0800

    springboot使用外部tomcat部署及简单的log日志配置

--- src/main/java/cn/lemon/boot/BootApplication.java
+++ src/main/java/cn/lemon/boot/BootApplication.java
@@ -1,16 +1,29 @@
 package cn.lemon.boot;
 
+import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("cn.lemon.boot.dao")
-public class BootApplication {
+public class BootApplication {//extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+//        //正常启动
         SpringApplication.run(BootApplication.class, args);
+
+        //外部启动
+//        new SpringApplicationBuilder(Application.class).web(WebApplicationType.SERVLET).run(args);
     }
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+//        return builder.sources(BootApplication.class);
+//    }
 
 }
